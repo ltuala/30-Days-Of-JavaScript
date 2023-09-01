6281ef6074a5d412f92a5b7338dc3e0988af7eab
ltuala <lyndontuala28@gmail.com>
2023-09-01 21:43:36 +0800

    Day 1     Learned how to     - define javascript in html     - how to declare variable     - assign different datatype to variable     - log output to browser console

---
+++ 01_Day_Introduction/sandbox/datatypes.js
@@ -0,0 +1,31 @@
+console.log(typeof stringVar)
+console.log(typeof booleanVar)
+console.log(typeof undefinedVar)
+console.log(typeof nullVar)
+
+let firstVarUnassigned
+let secondVarUnassigned
+let thirdVarUnassigned
+let fourthVarUnassigned
+
+let firstVarAssigned = 'string'
+let secondVarAssigned = true
+let thirdVarAssigned = undefined
+let fourthVarAssigned = null
+
+let firstName = 'lyndon'
+let lastName = 'tuala'
+let maritalStatus = 'single'
+let country = 'ph'
+let age = 25
+
+let firstNameS = 'lyndon', lastNameS = 'tuala', maritalStatusS = 'single', countryS = 'ph', ageS = 25
+
+let myAge
+let yourAge
+
+myAge = 25
+yourAge = 30
+
+console.log('I am', myAge, 'years old.')
+console.log('You are', yourAge, 'years old.')
---
+++ 01_Day_Introduction/sandbox/variable.js
@@ -0,0 +1,4 @@
+let stringVar = 'string'
+let booleanVar = true
+let undefinedVar = undefined
+let nullVar = null
